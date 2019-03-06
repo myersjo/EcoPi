91c496721ae3473da56c069c1ee2d488216a619e
BColwell131 <bcolwell@tcd.ie>
2019-03-06 15:11:08 +0000

    Setup background

--- Dashboard/react-dashboard/src/components/app/App.js
+++ Dashboard/react-dashboard/src/components/app/App.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
 import './App.scss';
-
-//Testing material UI
-import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          {/* Testing material UI button */}
-          <Button variant="contained" color="primary">
-            Primary
-          </Button>
+          <p>Hello World!</p>
         </header>
       </div>
     );
